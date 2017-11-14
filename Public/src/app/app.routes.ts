a30b8ccd3d5b9abae685bfc9c9ad599563e755c0
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-11-13 21:23:19 -0500

    vistas usuario armasdas

--- Public/src/app/app.routes.ts
+++ Public/src/app/app.routes.ts
@@ -3,7 +3,9 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ClienteComponent }  from './components/home/cliente/cliente.component';
 import { UsuarioComponent } from './components/home/usuario/usuario.component';
-
+import { FleterosComponent } from './components/user/fleteros/fleteros.component';
+import {PerfilComponent} from './components/user/perfil/perfil.component';
+import {PublicarComponent} from './components/user/publicar/publicar.component';
 
 const APP_ROUTES: Routes = [
 
@@ -11,6 +13,9 @@ const APP_ROUTES: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'user', component: UsuarioComponent },
   { path: 'client', component: ClienteComponent },
+  { path: 'verFleteros', component: FleterosComponent },
+  { path: 'perfilUser', component: PerfilComponent },
+  { path: 'publicarFlete', component: PublicarComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
