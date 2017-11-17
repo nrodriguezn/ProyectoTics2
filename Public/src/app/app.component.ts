a14996d303556221df3a7396456e4a4a0195cd05
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-11-17 18:55:16 -0500

    cambios algunos, se sigue trabajando

--- Public/src/app/app.component.ts
+++ Public/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FletesService } from './services/fletes.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,14 @@ export class AppComponent implements OnInit {
 
   usuario:string
 
-  constructor( private _fletesService:FletesService){}
+  constructor( private activatedRoute: ActivatedRoute, private _fletesService:FletesService){}
 
   ngOnInit() {
+    //this.activatedRoute.params
+    //.subscribe( this.usuario => this.usuario = this._fletesService.getUserType())
+
+
       this.usuario = this._fletesService.getUserType()
   }
-    
+
 }
--- Public/src/app/components/login/login.component.ts
+++ Public/src/app/components/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   setUserTypeLogin(userType){
-    return this._fletesService.setUserType(userType)
+     return this._fletesService.setUserType(userType)
   }
 
 }
