becabb0d71a3ad4d1636e50ca629c2afa086f74b
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-11-20 19:12:25 -0500

    commit por si la cago

--- Public/src/app/components/client/profile/profile.component.ts
+++ Public/src/app/components/client/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from './../../../services/auth.service';
+import {FletesService} from './../../../services/fletes.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  userType:string
+  profile:any
+
+  constructor(public auth:AuthService, private _fletesService:FletesService) { }
 
   ngOnInit() {
+    this.userType = this._fletesService.getUserType()
+      if (this.auth.userProfile) {
+        this.profile = this.auth.userProfile;
+      } else {
+        this.auth.getProfile((err, profile) => {
+          this.profile = profile;
+        });
+      }
   }
 
 }
--- Public/src/app/components/home/home.component.ts
+++ Public/src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FletesService} from './../../services/fletes.service';
-
+import {AuthService} from './../../services/auth.service';
 
 
 @Component({
@@ -11,12 +11,21 @@ import {FletesService} from './../../services/fletes.service';
 export class HomeComponent implements OnInit {
 
 userType:string
+profile:any
 
-  constructor(private _fletesService:FletesService) { }
+  constructor(private _fletesService:FletesService, public auth:AuthService) { }
 
   ngOnInit() {
     this.userType = this._fletesService.getUserType()
-  }
+
+      if (this.auth.userProfile) {
+        this.profile = this.auth.userProfile;
+      } else {
+        this.auth.getProfile((err, profile) => {
+          this.profile = profile;
+        });
+      }
+    }
 
 
 
